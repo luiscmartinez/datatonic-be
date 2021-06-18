2e29f5a035a8d95274223649de01a9234402fada
Luis Martinez <mrluismartinezzz@gmail.com>
2021-06-17 17:45:03 -0700

    removing domain route

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -5,7 +5,7 @@ const corsOptions = {
   origin: [
     'http://localhost:3000',
     'https://datatonic.luismartinez.dev',
-    'https://datatonic.netlify.app/dashboard'
+    'https://datatonic.netlify.app'
   ],
 }
 module.exports = (app) => {
